02172ebf80b1c49d9da974dd936313989ff094be
Bajra Manandhar <32059486+bajra-manandhar17@users.noreply.github.com>
2021-07-09 17:05:54 +0545

    Imported UploadForm from Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Title from './components/Title'
+import UploadForm from './components/UploadForm'
 
 function App() {
   return (
     <div className='App'>
       <Title />
+      <UploadForm />
     </div>
   )
 }
