3ca338dfb4f9c5c10d63f5642f669303c86c9913
Bajra Manandhar <32059486+bajra-manandhar17@users.noreply.github.com>
2021-07-09 16:48:41 +0545

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyC7Zhq3lPTJq4gybVUgvzxBnwfolzIudhY',
+  authDomain: 'photo-gallery-react-2900d.firebaseapp.com',
+  projectId: 'photo-gallery-react-2900d',
+  storageBucket: 'photo-gallery-react-2900d.appspot.com',
+  messagingSenderId: '944841034093',
+  appId: '1:944841034093:web:5af1558c7830132d247264',
+}
+//Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+//Initialize and Exporting Storage and FireStorage
+const projectStorage = firebase.storage()
+const projectFireStore = firebase.firestore()
+
+export { projectStorage, projectFireStore }
