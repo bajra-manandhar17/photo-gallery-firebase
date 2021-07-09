b7acb13945d005677282d848a97f781fa279e2c8
Bajra Manandhar <32059486+bajra-manandhar17@users.noreply.github.com>
2021-07-09 17:05:30 +0545

    Created Upload Form with Error Handling

---
+++ src/components/UploadForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+const UploadForm = () => {
+  //Use State for file and error generating
+  const [file, setFile] = useState(null)
+  const [error, setError] = useState(null)
+
+  //Allowed Image Types
+  const types = ['image/png', 'image/jpeg', 'image/jpg']
+
+  //Function for uploading and monitoring images
+  const changeHandler = (e) => {
+    let selected = e.target.files[0]
+
+    //check image
+    if (selected && types.includes(selected.type)) {
+      setFile(selected)
+      setError('')
+    } else {
+      setFile(null)
+      setError('Please Select an Image File (png or jpeg/jpg)')
+    }
+  }
+
+  //Input button redirecting to changeHandler function when invoked
+  //output class to check and output error if type is not mentioned in the array
+  return (
+    <form>
+      <input type='file' onChange={changeHandler} />
+      <div className='output'>
+        {error & <div className='error'>{error}</div>}
+      </div>
+    </form>
+  )
+}
+
+export default UploadForm
