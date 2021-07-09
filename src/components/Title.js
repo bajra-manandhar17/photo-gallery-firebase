6df54f662759103c7acc4bb7ba02b36fe0a8005d
Bajra Manandhar <32059486+bajra-manandhar17@users.noreply.github.com>
2021-07-09 12:44:56 +0545

    Initializd Project Files

---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div className='title'>
+      <h1>Photo Gallery</h1>
+      <h2>Your Photos</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
+        dolores?
+      </p>
+    </div>
+  )
+}
+
+export default Title
